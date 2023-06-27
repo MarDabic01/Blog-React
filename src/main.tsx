1520e98c197d0ff9ffc9c28df291e4ec05fe9039
Marko Dabic <marko.dabic@itengine.rs>
2023-06-27 09:46:47 +0200

    Setting up routing and chakra ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,18 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import router from "./routing/route";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode></React.StrictMode>
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <RouterProvider router={router} />
+      </ChakraProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
 );
